3ec36b593a6cb8fc190b7af09a22ee8592424d7e
Cédric Belin <cedric@belin.io>
2019-08-27 19:55:04 +0200

    Moved the MkDocs settings to the "doc" folder

--- gulpfile.ts
+++ gulpfile.ts
@@ -35,7 +35,7 @@ task('doc', async () => {
   for (const path of ['CHANGELOG.md', 'LICENSE.md']) await copyFile(path, `doc/about/${path.toLowerCase()}`);
   await _exec('typedoc', ['--gaID', process.env.GOOGLE_ANALYTICS_ID!, '--options', 'etc/typedoc.json', '--tsconfig', 'src/tsconfig.json']);
   await _exec('mkdocs', ['build', '--config-file=etc/mkdocs.yaml']);
-  return del(['doc/about/changelog.md', 'doc/about/license.md']);
+  return del(['doc/about/changelog.md', 'doc/about/license.md', 'web/mkdocs.yaml']);
 });
 
 /** Fixes the coding standards issues. */
