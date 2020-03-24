8c6f83eb5ef778870a6725c0a752de3a6d71f22a
Cédric Belin <cedric@belin.io>
2020-03-24 15:00:30 +0100

    Replaced `chai` by Node `assert` for testing

--- test/finder_test.js
+++ test/finder_test.js
@@ -1,40 +1,38 @@
-import chai from 'chai';
+import {strict as assert} from 'assert';
 import {delimiter} from 'path';
 import {Finder} from '../lib/index.js';
 
 /** Tests the features of the {@link Finder} class. */
 describe('Finder', () => {
-  const {expect} = chai;
-
   describe('constructor', () => {
     it('should set the `path` property to the value of the `PATH` environment variable by default', () => {
       const pathEnv = 'PATH' in process.env ? process.env.PATH : '';
       const path = pathEnv.length ? pathEnv.split(delimiter) : [];
-      expect(new Finder().path).to.have.ordered.members(path);
+      assert.deepEqual(new Finder().path, path);
     });
 
     it('should split the input path using the path separator', () => {
       const path = ['/usr/local/bin', '/usr/bin'];
-      expect(new Finder({path: path.join(delimiter)}).path).to.have.ordered.members(path);
+      assert.deepEqual(new Finder({path: path.join(delimiter)}).path, path);
     });
 
     it('should set the `extensions` property to the value of the `PATHEXT` environment variable by default', () => {
       const pathExt = 'PATHEXT' in process.env ? process.env.PATHEXT : '';
       const extensions = pathExt.length ? pathExt.split(delimiter).map(item => item.toLowerCase()) : [];
-      expect(new Finder().extensions).to.have.ordered.members(extensions);
+      assert.deepEqual(new Finder().extensions, extensions);
     });
 
     it('should split the extension list using the path separator', () => {
       const extensions = ['.EXE', '.CMD', '.BAT'];
-      expect(new Finder({extensions: extensions.join(delimiter)}).extensions).to.have.ordered.members(['.exe', '.cmd', '.bat']);
+      assert.deepEqual(new Finder({extensions: extensions.join(delimiter)}).extensions, ['.exe', '.cmd', '.bat']);
     });
 
     it('should set the `pathSeparator` property to the value of the `path.delimiter` constant by default', () => {
-      expect(new Finder().pathSeparator).to.equal(delimiter);
+      assert.equal(new Finder().pathSeparator, delimiter);
     });
 
     it('should properly set the path separator', () => {
-      expect(new Finder({pathSeparator: '#'}).pathSeparator).to.equal('#');
+      assert.equal(new Finder({pathSeparator: '#'}).pathSeparator, '#');
     });
   });
 
@@ -47,28 +45,28 @@ describe('Finder', () => {
 
     it('should return the path of the `executable.cmd` file on Windows', async () => {
       const executables = await toArray(new Finder({path: 'test/fixtures'}).find('executable'));
-      expect(executables).to.have.lengthOf(Finder.isWindows ? 1 : 0);
-      if (Finder.isWindows) expect(executables[0].endsWith('\\test\\fixtures\\executable.cmd')).to.be.true;
+      assert.equal(executables.length, Finder.isWindows ? 1 : 0);
+      if (Finder.isWindows) assert(executables[0].endsWith('\\test\\fixtures\\executable.cmd'));
     });
 
     it('should return the path of the `executable.sh` file on POSIX', async () => {
       const executables = await toArray(new Finder({path: 'test/fixtures'}).find('executable.sh'));
-      expect(executables).to.have.lengthOf(Finder.isWindows ? 0 : 1);
-      if (!Finder.isWindows) expect(executables[0].endsWith('/test/fixtures/executable.sh')).to.be.true;
+      assert.equal(executables.length, Finder.isWindows ? 0 : 1);
+      if (!Finder.isWindows) assert(executables[0].endsWith('/test/fixtures/executable.sh'));
     });
   });
 
   describe('.isExecutable()', () => {
     it('should return `false` for a non-executable file', async () => {
-      expect(await new Finder().isExecutable('AUTHORS.txt')).to.be.false;
+      assert.equal(await new Finder().isExecutable('AUTHORS.txt'), false);
     });
 
     it('should return `false` for a POSIX executable, when test is run on Windows', async () => {
-      expect(await new Finder().isExecutable('test/fixtures/executable.sh')).to.not.equal(Finder.isWindows);
+      assert.notEqual(await new Finder().isExecutable('test/fixtures/executable.sh'), Finder.isWindows);
     });
 
     it('should return `false` for a Windows executable, when test is run on POSIX', async () => {
-      expect(await new Finder().isExecutable('test/fixtures/executable.cmd')).to.equal(Finder.isWindows);
+      assert.equal(await new Finder().isExecutable('test/fixtures/executable.cmd'), Finder.isWindows);
     });
   });
 });
--- test/which_test.js
+++ test/which_test.js
@@ -1,76 +1,79 @@
-import chai from 'chai';
+import {strict as assert} from 'assert';
 import {Finder, FinderError, which} from '../lib/index.js';
 
 /** Tests the features of the {@link which} function. */
 describe('which()', () => {
-  const {expect} = chai;
-
   it('should return the path of the `executable.cmd` file on Windows', async () => {
     try {
       const executable = await which('executable', {all: false, path: 'test/fixtures'});
-      if (Finder.isWindows) expect(executable.endsWith('\\test\\fixtures\\executable.cmd')).to.be.true;
-      else expect.fail('Error not thrown');
+      if (Finder.isWindows) assert(executable.endsWith('\\test\\fixtures\\executable.cmd'));
+      else assert.fail('Error not thrown');
     }
 
     catch (err) {
-      if (Finder.isWindows) expect.fail(err.message);
-      else expect(err).to.be.an.instanceof(FinderError);
+      if (Finder.isWindows) assert.fail(err.message);
+      else assert(err instanceof FinderError);
     }
   });
 
   it('should return all the paths of the `executable.cmd` file on Windows', async () => {
     try {
       const executables = await which('executable', {all: true, path: 'test/fixtures'});
-      if (!Finder.isWindows) expect.fail('Error not thrown');
+      if (!Finder.isWindows) assert.fail('Error not thrown');
       else {
-        expect(executables).to.be.an('array').and.have.lengthOf(1);
-        expect(executables[0]).to.be.a('string').and.match(/\\test\\fixtures\\executable\.cmd$/);
+        assert(Array.isArray(executables));
+        assert.equal(executables.length, 1);
+        assert.equal(typeof executables[0], 'string');
+        assert.match(executables[0], /\\test\\fixtures\\executable\.cmd$/);
       }
     }
 
     catch (err) {
-      if (Finder.isWindows) expect.fail(err.message);
-      else expect(err).to.be.an.instanceof(FinderError);
+      if (Finder.isWindows) assert.fail(err.message);
+      else assert(err instanceof FinderError);
     }
   });
 
   it('should return the path of the `executable.sh` file on POSIX', async () => {
     try {
       const executable = await which('executable.sh', {all: false, path: 'test/fixtures'});
-      if (Finder.isWindows) expect.fail('Error not thrown');
-      else expect(executable.endsWith('/test/fixtures/executable.sh')).to.be.true;
+      if (Finder.isWindows) assert.fail('Error not thrown');
+      else assert(executable.endsWith('/test/fixtures/executable.sh'));
     }
 
     catch (err) {
-      if (Finder.isWindows) expect(err).to.be.an.instanceof(FinderError);
-      else expect.fail(err.message);
+      if (Finder.isWindows) assert(err instanceof FinderError);
+      else assert.fail(err.message);
     }
   });
 
   it('should return all the paths of the `executable.sh` file on POSIX', async () => {
     try {
       const executables = await which('executable.sh', {all: true, path: 'test/fixtures'});
-      if (Finder.isWindows) expect.fail('Error not thrown');
+      if (Finder.isWindows) assert.fail('Error not thrown');
       else {
-        expect(executables).to.be.an('array').and.have.lengthOf(1);
-        expect(executables[0]).to.be.a('string').and.match(/\/test\/fixtures\/executable\.sh$/);
+        assert(Array.isArray(executables));
+        assert.equal(executables.length, 1);
+        assert.equal(typeof executables[0], 'string');
+        assert.match(executables[0], /\/test\/fixtures\/executable\.sh$/);
       }
     }
 
     catch (err) {
-      if (Finder.isWindows) expect(err).to.be.an.instanceof(FinderError);
-      else expect.fail(err.message);
+      if (Finder.isWindows) assert(err instanceof FinderError);
+      else assert.fail(err.message);
     }
   });
 
   it('should return the value of the `onError` handler', async () => {
     const executable = await which('executable', {all: false, onError: () => 'foo', path: 'test/fixtures'});
-    if (!Finder.isWindows) expect(executable).to.equal('foo');
+    if (!Finder.isWindows) assert.equal(executable, 'foo');
 
     const executables = await which('executable.sh', {all: true, onError: () => ['foo'], path: 'test/fixtures'});
     if (Finder.isWindows) {
-      expect(executables).to.be.an('array').and.have.lengthOf(1);
-      expect(executables[0]).to.equal('foo');
+      assert(Array.isArray(executables));
+      assert.equal(executables.length, 1);
+      assert.equal(executables[0], 'foo');
     }
   });
 });
