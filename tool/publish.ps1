d94b07d0de473061a94a8f822076971f55bc3cab
Cédric Belin <cedric@belin.io>
2020-05-22 00:34:26 +0200

    Updated the "publish" task

--- tool/publish.ps1
+++ tool/publish.ps1
@@ -4,3 +4,7 @@ Set-Location (Split-Path $PSScriptRoot)
 
 tool/clean.ps1
 npm publish
+
+$version = (Get-Content package.json | ConvertFrom-Json).version
+git tag "v$version"
+git push origin "v$version"
