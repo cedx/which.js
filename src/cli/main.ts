8366835c6a1a3d7939f8497d0569e1d1a0c22e02
Cédric Belin <cedric@belin.io>
2021-11-20 14:03:45 +0100

    Updated the API reference

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,4 +1,4 @@
-import program from "commander";
+import {program} from "commander";
 import {which} from "../which.js";
 import {packageVersion} from "./version.g.js";
 
@@ -11,24 +11,26 @@ export async function main(): Promise<void> {
 	process.title = "Which.js";
 
 	// Parse the command line arguments.
+	let executable;
 	program.name("which")
 		.description("Find the instances of an executable in the system path.")
 		.version(packageVersion, "-v, --version")
 		.option("-a, --all", "list all instances of executables found (instead of just the first one)")
 		.option("-s, --silent", "silence the output, just return the exit code (0 if any executable is found, otherwise 1)")
-		.arguments("<command>").action(command => program.executable = command)
+		.arguments("<command>").action(command => executable = command)
 		.parse(process.argv);
 
-	if (!program.executable) {
+	const options = program.opts();
+	if (!executable) {
 		program.outputHelp();
 		process.exitCode = 64;
 		return;
 	}
 
 	// Run the program.
-	let executables = await which(program.executable, {all: program.all});
-	if (!program.silent) {
-		if (!Array.isArray(executables)) executables = [executables];
-		for (const path of executables) console.log(path); // eslint-disable-line no-console
+	let paths = await which(executable, {all: options.all});
+	if (!options.silent) {
+		if (!Array.isArray(paths)) paths = [paths];
+		for (const path of paths) console.log(path);
 	}
 }
--- tool/doc.ps1
+++ tool/doc.ps1
@@ -1,6 +1,4 @@
 #!/usr/bin/env pwsh
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
-
-Copy-Item doc/img/favicon.ico doc/api
-mkdocs build --config-file=etc/mkdocs.yaml
+Copy-Item www/favicon.ico docs
