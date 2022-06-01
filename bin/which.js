b964ff19130a332757f2458d2dc852cc07e2b269
Cédric Belin <cedric@belin.io>
2022-06-01 20:25:39 +0200

    Ported the CLI to JS

--- bin/which.js
+++ bin/which.js
@@ -1,12 +1,33 @@
 #!/usr/bin/env node
-import {FinderError} from "../lib/index.js";
-import {main} from "../lib/cli/main.js";
+import console from "node:console";
+import process from "node:process";
+import {readFileSync} from "node:fs";
+import {program} from "commander";
+import which from "../lib/index.js";
+
+// Parse the command line arguments.
+const {version} = JSON.parse(readFileSync(new URL("../package.json", import.meta.url), "utf8"));
+program.name("which")
+	.description("Find the instances of an executable in the system path.")
+	.version(version, "-v, --version")
+	.option("-a, --all", "list all executable instances found (instead of just the first one)")
+	.option("-s, --silent", "silence the output, just return the exit code (0 if any executable is found, otherwise 1)")
+	.argument("<command>")
+	.parse();
 
 // Start the application.
-main().catch(err => {
-	if (err instanceof FinderError) process.exitCode = 1;
-	else {
-		console.error(err.message);
-		process.exitCode = 2;
+const [command] = program.args;
+const {all, silent} = program.opts();
+
+try {
+	const finder = which(command);
+	let paths = await (all ? finder.all() : finder.first());
+	if (!silent) {
+		if (!Array.isArray(paths)) paths = [paths];
+		paths.forEach(path => console.log(path));
 	}
-});
+}
+catch (error) {
+	if (!silent) console.error(error instanceof Error ? error.message : error);
+	process.exitCode = 1;
+}
--- example/main.js
+++ example/main.js
@@ -1,15 +1,10 @@
+import console from "node:console";
 import which from "@cedx/which";
 
-/**
- * Finds the instances of an executable.
- */
 try {
-	// `path` is the absolute path to the executable.
 	const path = await which("foobar").first();
 	console.log(`The command 'foobar' is located at: ${path}`);
 }
-
 catch (error) {
-	// `error` is an instance of `FinderError`.
-	console.log("The command 'foobar' has not been found.");
+	console.error("The command 'foobar' has not been found.");
 }
