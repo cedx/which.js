af43d4c3cb9bdd60fbbb64d46d6d0745626a9f12
Cédric Belin <cedric@belin.io>
2022-06-29 10:23:50 +0200

    Using import assertions

--- bin/which.js
+++ bin/which.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 import console from "node:console";
 import process from "node:process";
-import {readFileSync} from "node:fs";
 import {program} from "commander";
+import pkg from "../package.json" assert {type: "json"};
 import which from "../lib/index.js";
 
 // Parse the command line arguments.
-const {version} = JSON.parse(readFileSync(new URL("../package.json", import.meta.url), "utf8"));
 program.name("which")
 	.description("Find the instances of an executable in the system path.")
-	.version(version, "-v, --version")
+	.version(pkg.version, "-v, --version")
 	.option("-a, --all", "list all executable instances found (instead of just the first one)")
 	.option("-s, --silent", "silence the output, just return the exit code (0 if any executable is found, otherwise 1)")
 	.argument("<command>", "the name of the executable to find")
