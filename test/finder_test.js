af9bc8a136e9298ce5b579206a2b68061d247985
Cédric Belin <cedric@belin.io>
2022-07-15 12:42:30 +0200

    Using the new describe/it API

--- test/finder_test.js
+++ test/finder_test.js
@@ -1,69 +1,75 @@
 import assert from "node:assert/strict";
 import {delimiter} from "node:path";
 import {env} from "node:process";
-import test from "node:test";
+// @ts-expect-error TS2614
+import {describe, it} from "node:test";
 import {Finder} from "../src/index.js";
 
-test("Finder.constructor", async ctx => {
-	await ctx.test("should set the `paths` property to the value of the `PATH` environment variable by default", () => {
-		const pathEnv = env.PATH ?? "";
-		const paths = pathEnv ? pathEnv.split(Finder.isWindows ? ";" : delimiter) : [];
-		assert.deepEqual(new Finder().paths, paths);
+/**
+ * Tests the features of the {@link Finder} class.
+ */
+describe("Finder", () => {
+	describe("constructor", () => {
+		it("should set the `paths` property to the value of the `PATH` environment variable by default", () => {
+			const pathEnv = env.PATH ?? "";
+			const paths = pathEnv ? pathEnv.split(Finder.isWindows ? ";" : delimiter) : [];
+			assert.deepEqual(new Finder().paths, paths);
+		});
+
+		it("should set the `extensions` property to the value of the `PATHEXT` environment variable by default", () => {
+			const pathExt = env.PATHEXT ?? "";
+			const extensions = pathExt ? pathExt.split(";").map(item => item.toLowerCase()) : [".exe", ".cmd", ".bat", ".com"];
+			assert.deepEqual(new Finder().extensions, extensions);
+		});
+
+		it("should put in lower case the list of file extensions", () => {
+			assert.deepEqual(new Finder({extensions: [".EXE", ".JS", ".PS1"]}).extensions, [".exe", ".js", ".ps1"]);
+		});
 	});
 
-	await ctx.test("should set the `extensions` property to the value of the `PATHEXT` environment variable by default", () => {
-		const pathExt = env.PATHEXT ?? "";
-		const extensions = pathExt ? pathExt.split(";").map(item => item.toLowerCase()) : [".exe", ".cmd", ".bat", ".com"];
-		assert.deepEqual(new Finder().extensions, extensions);
+	describe(".find()", () => {
+		const finder = new Finder({paths: ["test/fixture"]});
+
+		async function toArray(/** @type {AsyncIterable<string>} */ asyncIterable) {
+			const items = [];
+			for await (const item of asyncIterable) items.push(item);
+			return items;
+		}
+
+		it("should return the path of the `executable.cmd` file on Windows", async () => {
+			const executables = await toArray(finder.find("executable"));
+			assert.equal(executables.length, Finder.isWindows ? 1 : 0);
+			if (Finder.isWindows) assert.ok(executables[0].endsWith("\\test\\fixture\\executable.cmd"));
+		});
+
+		it("should return the path of the `executable.sh` file on POSIX", async () => {
+			const executables = await toArray(finder.find("executable.sh"));
+			assert.equal(executables.length, Finder.isWindows ? 0 : 1);
+			if (!Finder.isWindows) assert.ok(executables[0].endsWith("/test/fixture/executable.sh"));
+		});
+
+		it("should return an empty array if the searched command is not executable or not found", async () => {
+			let executables = await toArray(finder.find("not_executable.sh"));
+			assert.equal(executables.length, 0);
+			executables = await toArray(finder.find("foo"));
+			assert.equal(executables.length, 0);
+		});
 	});
 
-	await ctx.test("should put in lower case the list of file extensions", () => {
-		assert.deepEqual(new Finder({extensions: [".EXE", ".JS", ".PS1"]}).extensions, [".exe", ".js", ".ps1"]);
-	});
-});
+	describe(".isExecutable()", () => {
+		const finder = new Finder;
 
-test("Finder.find()", async ctx => {
-	const finder = new Finder({paths: ["test/fixture"]});
+		it("should return `false` if the searched command is not executable or not found", async () => {
+			assert.equal(await finder.isExecutable("test/fixture/not_executable.sh"), false);
+			assert.equal(await finder.isExecutable("foo/bar/baz.qux"), false);
+		});
 
-	async function toArray(/** @type {AsyncIterable<string>} */ asyncIterable) {
-		const items = [];
-		for await (const item of asyncIterable) items.push(item);
-		return items;
-	}
+		it("should return `false` for a POSIX executable, when test is run on Windows", async () => {
+			assert.notEqual(await finder.isExecutable("test/fixture/executable.sh"), Finder.isWindows);
+		});
 
-	await ctx.test("should return the path of the `executable.cmd` file on Windows", async () => {
-		const executables = await toArray(finder.find("executable"));
-		assert.equal(executables.length, Finder.isWindows ? 1 : 0);
-		if (Finder.isWindows) assert.ok(executables[0].endsWith("\\test\\fixture\\executable.cmd"));
-	});
-
-	await ctx.test("should return the path of the `executable.sh` file on POSIX", async () => {
-		const executables = await toArray(finder.find("executable.sh"));
-		assert.equal(executables.length, Finder.isWindows ? 0 : 1);
-		if (!Finder.isWindows) assert.ok(executables[0].endsWith("/test/fixture/executable.sh"));
-	});
-
-	await ctx.test("should return an empty array if the searched command is not executable or not found", async () => {
-		let executables = await toArray(finder.find("not_executable.sh"));
-		assert.equal(executables.length, 0);
-		executables = await toArray(finder.find("foo"));
-		assert.equal(executables.length, 0);
-	});
-});
-
-test("Finder.isExecutable()", async ctx => {
-	const finder = new Finder;
-
-	await ctx.test("should return `false` if the searched command is not executable or not found", async () => {
-		assert.equal(await finder.isExecutable("test/fixture/not_executable.sh"), false);
-		assert.equal(await finder.isExecutable("foo/bar/baz.qux"), false);
-	});
-
-	await ctx.test("should return `false` for a POSIX executable, when test is run on Windows", async () => {
-		assert.notEqual(await finder.isExecutable("test/fixture/executable.sh"), Finder.isWindows);
-	});
-
-	await ctx.test("should return `false` for a Windows executable, when test is run on POSIX", async () => {
-		assert.equal(await finder.isExecutable("test/fixture/executable.cmd"), Finder.isWindows);
+		it("should return `false` for a Windows executable, when test is run on POSIX", async () => {
+			assert.equal(await finder.isExecutable("test/fixture/executable.cmd"), Finder.isWindows);
+		});
 	});
 });
--- test/result_set_test.js
+++ test/result_set_test.js
@@ -1,65 +1,71 @@
 import assert from "node:assert/strict";
-import test from "node:test";
+// @ts-expect-error TS2614
+import {describe, it} from "node:test";
 import which, {Finder} from "../src/index.js";
 
-test("ResultSet.all()", async ctx => {
-	const options = {paths: ["test/fixture"]};
+/**
+ * Tests the features of the {@link ResultSet} class.
+ */
+describe("ResultSet", () => {
+	describe(".all()", () => {
+		const options = {paths: ["test/fixture"]};
 
-	await ctx.test("should return the path of the `executable.cmd` file on Windows", async () => {
-		const promise = which("executable", options).all();
-		if (!Finder.isWindows) assert.rejects(promise);
-		else {
-			const executables = await promise;
-			assert.ok(Array.isArray(executables));
-			assert.equal(executables.length, 1);
-			assert.equal(typeof executables[0], "string");
-			assert.ok(executables[0].endsWith("\\test\\fixture\\executable.cmd"));
-		}
+		it("should return the path of the `executable.cmd` file on Windows", async () => {
+			const promise = which("executable", options).all();
+			if (!Finder.isWindows) assert.rejects(promise);
+			else {
+				const executables = await promise;
+				assert.ok(Array.isArray(executables));
+				assert.equal(executables.length, 1);
+				assert.equal(typeof executables[0], "string");
+				assert.ok(executables[0].endsWith("\\test\\fixture\\executable.cmd"));
+			}
+		});
+
+		it("should return the path of the `executable.sh` file on POSIX", async () => {
+			const promise = which("executable.sh", options).all();
+			if (Finder.isWindows) assert.rejects(promise);
+			else {
+				const executables = await promise;
+				assert.ok(Array.isArray(executables));
+				assert.equal(executables.length, 1);
+				assert.equal(typeof executables[0], "string");
+				assert.ok(executables[0].endsWith("/test/fixture/executable.sh"));
+			}
+		});
+
+		it("should reject if the searched command is not executable or not found", () => {
+			assert.rejects(() => which("not_executable.sh", options).all());
+			assert.rejects(() => which("foo", options).all());
+		});
 	});
 
-	await ctx.test("should return the path of the `executable.sh` file on POSIX", async () => {
-		const promise = which("executable.sh", options).all();
-		if (Finder.isWindows) assert.rejects(promise);
-		else {
-			const executables = await promise;
-			assert.ok(Array.isArray(executables));
-			assert.equal(executables.length, 1);
-			assert.equal(typeof executables[0], "string");
-			assert.ok(executables[0].endsWith("/test/fixture/executable.sh"));
-		}
-	});
+	describe(".first()", () => {
+		const options = {paths: ["test/fixture"]};
 
-	await ctx.test("should reject if the searched command is not executable or not found", () => {
-		assert.rejects(() => which("not_executable.sh", options).all());
-		assert.rejects(() => which("foo", options).all());
-	});
-});
+		it("should return the path of the `executable.cmd` file on Windows", async () => {
+			const promise = which("executable", options).first();
+			if (!Finder.isWindows) assert.rejects(promise);
+			else {
+				const executable = await promise;
+				assert.equal(typeof executable, "string");
+				assert.ok(executable.endsWith("\\test\\fixture\\executable.cmd"));
+			}
+		});
 
-test("ResultSet.first()", async ctx => {
-	const options = {paths: ["test/fixture"]};
+		it("should return the path of the `executable.sh` file on POSIX", async () => {
+			const promise = which("executable.sh", options).first();
+			if (Finder.isWindows) assert.rejects(promise);
+			else {
+				const executable = await promise;
+				assert.equal(typeof executable, "string");
+				assert.ok(executable.endsWith("/test/fixture/executable.sh"));
+			}
+		});
 
-	await ctx.test("should return the path of the `executable.cmd` file on Windows", async () => {
-		const promise = which("executable", options).first();
-		if (!Finder.isWindows) assert.rejects(promise);
-		else {
-			const executable = await promise;
-			assert.equal(typeof executable, "string");
-			assert.ok(executable.endsWith("\\test\\fixture\\executable.cmd"));
-		}
-	});
-
-	await ctx.test("should return the path of the `executable.sh` file on POSIX", async () => {
-		const promise = which("executable.sh", options).first();
-		if (Finder.isWindows) assert.rejects(promise);
-		else {
-			const executable = await promise;
-			assert.equal(typeof executable, "string");
-			assert.ok(executable.endsWith("/test/fixture/executable.sh"));
-		}
-	});
-
-	await ctx.test("should reject if the searched command is not executable or not found", () => {
-		assert.rejects(() => which("not_executable.sh", options).first());
-		assert.rejects(() => which("foo", options).first());
+		it("should reject if the searched command is not executable or not found", () => {
+			assert.rejects(() => which("not_executable.sh", options).first());
+			assert.rejects(() => which("foo", options).first());
+		});
 	});
 });
