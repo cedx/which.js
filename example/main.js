f61c15e773fcdd4e7255ca95e3d2e07892ea803b
Cédric Belin <cedric@belin.io>
2022-10-08 22:42:40 +0200

    Updated the settings of the static analyzer

--- example/main.js
+++ example/main.js
@@ -3,8 +3,8 @@ import which from "@cedx/which";
 
 try {
 	const path = await which("foobar").first();
-	console.log(`The command 'foobar' is located at: ${path}`);
+	console.log(`The 'foobar' command is located at: ${path}`);
 }
-catch (error) {
-	console.error("The command 'foobar' has not been found.");
+catch {
+	console.error("The 'foobar' command has not been found.");
 }
