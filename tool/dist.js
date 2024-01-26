2489d4f2ebeaf8db58d387772e825726a565905d
Cédric Belin <cedric@belin.io>
2024-01-26 22:44:29 +0100

    Update the build system [skip ci]

--- tool/dist.js
+++ tool/dist.js
@@ -1,6 +1,6 @@
-import {shebang} from "./tools.js";
+import {readFileSync, writeFileSync} from "node:fs";
+import {EOL} from "node:os";
 
-/**
- * Packages the project.
- */
-shebang("bin/which.js");
+// Packages the project.
+const file = "bin/which.js";
+writeFileSync(file, `#!/usr/bin/env node${EOL}${readFileSync(file, {encoding: "utf8"})}`);
