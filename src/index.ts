3c2ae039cc7826b60e613965855c0bd8b4422629
Cédric Belin <cedric@belin.io>
2024-01-26 16:18:57 +0100

    Code formatting [skip ci]

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-export * from "./finder.js";
-export * from "./result_set.js";
 import {Finder, type FinderOptions} from "./finder.js";
 import {ResultSet} from "./result_set.js";
+
+export * from "./finder.js";
+export * from "./result_set.js";
 
 /**
  * Finds the instances of the specified command in the system path.
