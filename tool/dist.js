58f7cf5fa9909da650eb8526306d2bee4be4e8fb
Cédric Belin <cedric@belin.io>
2024-01-28 12:40:56 +0100

    Optimize the build stage [skip ci]

--- tool/dist.js
+++
@@ -1,6 +0,0 @@
-import {readFileSync, writeFileSync} from "node:fs";
-import {EOL} from "node:os";
-
-// Packages the project.
-const file = "bin/which.js";
-writeFileSync(file, `#!/usr/bin/env node${EOL}${readFileSync(file, {encoding: "utf8"})}`);
