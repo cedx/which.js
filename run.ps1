0cc4158781b4a71d6605d9977a51f421487517bc
Cédric Belin <cedric@belin.io>
2025-05-14 20:25:37 +0200

    Update the launch script [skip ci]

--- run.ps1
+++ run.ps1
@@ -1,8 +1,6 @@
 #!/usr/bin/env pwsh
 Set-StrictMode -Version Latest
+
 $commandPath = Get-Item $PSCommandPath
 $scriptRoot = $commandPath.LinkType ? (Split-Path $commandPath.LinkTarget) : $PSScriptRoot
-
-$entryPoint = "$scriptRoot/lib/Program.js"
-if (Test-Path "$entryPoint.map") { & node --enable-source-maps $entryPoint @args }
-else { & node $entryPoint @args }
+& node "$scriptRoot/lib/Program.js" @args
