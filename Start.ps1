0510e005abed0763c4430808a37ccf4a1f593f94
Cédric Belin <cedric.belin@mc2it.com>
2026-01-19 11:15:04 +0100

    Remove the launch script [skip ci]

--- Start.ps1
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env pwsh
-$ErrorActionPreference = "Stop"
-$PSNativeCommandUseErrorActionPreference = $true
-Set-StrictMode -Version Latest
-
-$package = Get-Content "$PSScriptRoot/package.json" | ConvertFrom-Json
-node $package.bin.Which @args
